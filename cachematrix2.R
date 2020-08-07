39fb209daa57b836a00170902446245d0a58d204
Actualiteit <vavdwal@outlook.com>
2020-08-07 20:34:29 +0200

    cleanup

--- cachematrix2.R
+++ cachematrix2.R
@@ -1,9 +1,3 @@
-## Function which calculates and caches the inverse of matrices.
-
-# makeCacheMatrix creates a special object from the provided matrix.
-# This object consists of a list of functions to get, manipulate and calculate
-# the provided matrix.
-
 makeCacheMatrix <- function(x = matrix()) {
   if (ncol(x) != nrow(x)){
     stop("The provided matrix is not inversible. please provide a square matrix.")
@@ -24,11 +18,6 @@ makeCacheMatrix <- function(x = matrix()) {
        setInv = setInv,
        getInv = getInv)
 }
-# cacheSolve checks if "Inv" already contains the inverse of the matrix.
-# if "Inv" is not "NULL", it prints the inverse from "Inv".
-# if "Inv" is "NULL", it calculates the inverse by applying the function solve()
-# to the matrix stored in "data", Storing the output in "Inv".
-
 cacheSolve <- function(x, ...) {
   m <- x$getInv
   if (!is.null(m)) {
